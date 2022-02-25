ada8cab6d2811554b5645d003ca6905b3a60bbfc
Sasha Franchini <sashafranchini@gmail.com>
2022-02-24 22:03:04 -0300

    gh-pages

--- script.js
+++ script.js
@@ -2,17 +2,9 @@
 let btns = document.querySelectorAll('#btn');
 let col = document.querySelectorAll('.col');
 
-// btns.forEach((btn)=>{
-//     btn.addEventListener('click', function(){    
-//         let ejemplo = document.querySelectorAll('#ejemplo');
-//         ejemplo.forEach((ejemplo)=>{
-//             ejemplo.classList.toggle("visible");
-//          });
-//     });
-// });
-let columnas = 0;
-    for(let i =0; i<btns.length; i++){
-        if(btns[i].addEventListener('click', function(){ 
+
+    for(let i =0; i<col.length; i++){
+        if(col[i].addEventListener('click', function(){ 
             let ejemplo = document.querySelectorAll("#ejemplo");
             ejemplo[i].classList.toggle("visible");
          }));
